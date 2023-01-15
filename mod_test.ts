070145f4d95109a651caf169e45ae641f4223f0e
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2023-01-15 23:14:02 +0100

    chore: remove unused test files

--- mod_test.ts
+++
@@ -1,5 +0,0 @@
-import { test, assertEquals } from "./test_deps.ts";
-
-test(function test_placeholder() {
-  assertEquals(true, true);
-});
--- test_deps.ts
+++
@@ -1,2 +0,0 @@
-export { test, runTests } from "https://deno.land/std@v0.32.0/testing/mod.ts";
-export { assertEquals } from "https://deno.land/std@v0.32.0/testing/asserts.ts";
